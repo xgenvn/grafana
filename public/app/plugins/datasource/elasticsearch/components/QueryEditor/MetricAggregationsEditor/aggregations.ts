8ec5ac4ebf3e83302bade7a14589b7bf01236967
Elfo404 <me@giordanoricci.com>
2021-04-07 14:56:19 +0100

    format MovingAverage settings as numbers

--- public/app/plugins/datasource/elasticsearch/components/QueryEditor/MetricAggregationsEditor/aggregations.ts
+++ public/app/plugins/datasource/elasticsearch/components/QueryEditor/MetricAggregationsEditor/aggregations.ts
@@ -169,8 +169,8 @@ export interface MovingAverageModelOption {
 
 export interface BaseMovingAverageModelSettings {
   model: MovingAverageModel;
-  window: number;
-  predict: number;
+  window: string;
+  predict: string;
 }
 
 export interface MovingAverageSimpleModelSettings extends BaseMovingAverageModelSettings {
@@ -183,15 +183,17 @@ export interface MovingAverageLinearModelSettings extends BaseMovingAverageModel
 
 export interface MovingAverageEWMAModelSettings extends BaseMovingAverageModelSettings {
   model: 'ewma';
-  alpha: number;
+  settings?: {
+    alpha?: string;
+  };
   minimize: boolean;
 }
 
 export interface MovingAverageHoltModelSettings extends BaseMovingAverageModelSettings {
   model: 'holt';
   settings: {
-    alpha?: number;
-    beta?: number;
+    alpha?: string;
+    beta?: string;
   };
   minimize: boolean;
 }
@@ -199,10 +201,10 @@ export interface MovingAverageHoltModelSettings extends BaseMovingAverageModelSe
 export interface MovingAverageHoltWintersModelSettings extends BaseMovingAverageModelSettings {
   model: 'holt_winters';
   settings: {
-    alpha?: number;
-    beta?: number;
-    gamma?: number;
-    period?: number;
+    alpha?: string;
+    beta?: string;
+    gamma?: string;
+    period?: string;
     pad?: boolean;
   };
   minimize: boolean;
@@ -235,6 +237,10 @@ export const isHoltWintersMovingAverage = (
   metric: MovingAverage | MovingAverage<'holt_winters'>
 ): metric is MovingAverage<'holt_winters'> => metric.settings?.model === 'holt_winters';
 
+export const isMovingAverageWithModelSettings = (
+  metric: MovingAverage
+): metric is MovingAverage<'ewma'> | MovingAverage<'holt'> | MovingAverage<'holt_winters'> =>
+  ['holt', 'ewma', 'holt_winters'].includes(metric.settings?.model || '');
 export interface MovingFunction extends BasePipelineMetricAggregation {
   type: 'moving_fn';
   settings?: {
