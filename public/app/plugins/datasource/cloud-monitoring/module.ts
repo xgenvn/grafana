32154f7f86e2fb84aa1fae5a1c78036192a34f7e
Erik Sundell <erik.sundell87@gmail.com>
2021-04-12 08:46:51 +0200

    move help file to cheat sheet

--- public/app/plugins/datasource/cloud-monitoring/module.ts
+++ public/app/plugins/datasource/cloud-monitoring/module.ts
@@ -1,12 +1,14 @@
 import { DataSourcePlugin } from '@grafana/data';
 import CloudMonitoringDatasource from './datasource';
 import { QueryEditor } from './components/QueryEditor';
+import CloudMonitoringCheatSheet from './components/CloudMonitoringCheatSheet';
 import { CloudMonitoringConfigCtrl } from './config_ctrl';
 import { CloudMonitoringAnnotationsQueryCtrl } from './annotations_query_ctrl';
 import { CloudMonitoringVariableQueryEditor } from './components/VariableQueryEditor';
 import { CloudMonitoringQuery } from './types';
 
 export const plugin = new DataSourcePlugin<CloudMonitoringDatasource, CloudMonitoringQuery>(CloudMonitoringDatasource)
+  .setQueryEditorHelp(CloudMonitoringCheatSheet)
   .setQueryEditor(QueryEditor)
   .setConfigCtrl(CloudMonitoringConfigCtrl)
   .setAnnotationQueryCtrl(CloudMonitoringAnnotationsQueryCtrl)
