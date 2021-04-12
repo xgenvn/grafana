a4f157aa447f5dcb784a2d9c975cc5f477160738
Arve Knudsen <arve.knudsen@gmail.com>
2021-04-12 10:39:58 +0200

    Add AWS Smithy configuration
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

---
+++ smithy/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("software.amazon.smithy").version("0.5.2")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("software.amazon.smithy:smithy-model:[1.0, 2.0[")
+}
---
+++ smithy/model/grafana.smithy
@@ -0,0 +1,6 @@
+namespace grafana
+
+/// The Grafana server.
+service Grafana {
+    version: "2021-04-07"
+}
